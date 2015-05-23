66844767b21b9ae44521ecf44e0f5ec4114dfbf1
thiefuniverse <sasaassssggggggggg@foxmail.com>
2015-05-23 15:05:18 +0800

    dsf
    zc

--- 电梯设计/Control.c
+++ 电梯设计/Control.c
@@ -155,3 +155,167 @@ void control_2(void)
 		}
 	}
 }
+
+void control_3(void)
+{
+	int tempFloor = 0;
+	int aimTempFloor_up = 0, aimTempFloor_down = 0;//临时目标楼层
+	int upTime = 0, downTime = 0;//当前上下行总时间
+	int tempFlag = 0;
+	/*状态为上时判断目标楼层*/
+	if (state == UP)
+	{
+		//遍历上部楼层
+		for (tempFloor = curFloor; tempFloor <= MAXFLOOR; tempFloor++)
+		{
+			upTime++;//上升一层时间加1
+			if (tempFlag==0&&(upCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				upTime += 3;//停靠一层时间加3
+				aimTempFloor_up = tempFloor;
+				tempFlag = 1;
+			}
+			if (tempFlag = 1 && (upCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				upTime += 3;//停靠一层时间加3
+			}
+		}
+
+		//遍历下部楼层
+		tempFlag = 0;
+		for (tempFloor = curFloor; tempFloor >= 1; tempFloor--)
+		{
+			downTime++;//下降一层时间加1
+			if (tempFlag==0&&(downCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				downTime += 3;//停靠一层时间加3
+				aimTempFloor_down = tempFloor;
+				tempFlag = 1;
+			}
+			if (tempFlag == 1 && (downCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				downTime += 3;//停靠一层时间加3
+			}
+
+		}
+		if (aimTempFloor_up == 0 && aimTempFloor_down == 0)
+			aimFloor = 0;//无上下行命令
+		else if (aimTempFloor_up==0&&aimTempFloor_down!=0)
+			aimFloor = aimTempFloor_down;//只有下行命令
+		else if (aimTempFloor_up!= 0 && aimTempFloor_down== 0)
+			aimFloor = aimTempFloor_up;//只有上行命令
+		else if ((aimTempFloor_up != 0 && aimTempFloor_down != 0)&&upTime * 5 >= downTime * 6)
+			aimFloor = aimTempFloor_down;//有上行下行,且符合调头命令
+		else if ((aimTempFloor_up != 0 && aimTempFloor_down != 0) && upTime * 5 < downTime * 6)
+			aimFloor = aimTempFloor_up;//有上行下行,且不符合调头命令
+
+		return;
+	}
+	else if (state == DOWN)
+	{
+		//遍历上部楼层
+		for (tempFloor = curFloor; tempFloor <= MAXFLOOR; tempFloor++)
+		{
+			upTime++;//上升一层时间加1
+			if (tempFlag == 0 && (upCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				upTime += 3;//停靠一层时间加3
+				aimTempFloor_up = tempFloor;
+				tempFlag = 1;
+			}
+			if (tempFlag = 1 && (upCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				upTime += 3;//停靠一层时间加3
+			}
+		}
+
+		//遍历下部楼层
+		tempFlag = 0;
+		for (tempFloor = curFloor; tempFloor >= 1; tempFloor--)
+		{
+			downTime++;//下降一层时间加1
+			if (tempFlag == 0 && (downCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				downTime += 3;//停靠一层时间加3
+				aimTempFloor_down = tempFloor;
+				tempFlag = 1;
+			}
+			if (tempFlag == 1 && (downCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				downTime += 3;//停靠一层时间加3
+			}
+
+		}
+		if (aimTempFloor_up == 0 && aimTempFloor_down == 0)
+			aimFloor = 0;//无上下行命令
+		else if (aimTempFloor_up == 0 && aimTempFloor_down != 0)
+			aimFloor = aimTempFloor_down;//只有下行命令
+		else if (aimTempFloor_up != 0 && aimTempFloor_down == 0)
+			aimFloor = aimTempFloor_up;//只有上行命令
+		else if ((aimTempFloor_up != 0 && aimTempFloor_down != 0) && downTime * 5 >= upTime * 6)
+			aimFloor = aimTempFloor_down;//有上行下行,且符合调头命令
+		else if ((aimTempFloor_up != 0 && aimTempFloor_down != 0) && downTime * 5 < upTime * 6)
+			aimFloor = aimTempFloor_up;//有上行下行,且不符合调头命令
+
+		return;
+	}
+	else if (state == VACANT)
+	{
+		//接收到一个楼层指令后，立马作为目标楼层
+		for (tempFloor = 1; tempFloor <= MAXFLOOR; tempFloor++)
+		{
+			if (downCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE || upCmd[tempFloor - 1] == TRUE)
+			{
+				aimFloor = tempFloor;
+				return;
+			}
+		}
+		aimFloor = 0;//Maybe it's wrong.!!!!!!!!!!!!
+		return;
+	}
+	else if (state == PAUSE)
+	{
+		//遍历上部楼层
+		for (tempFloor = curFloor; tempFloor <= MAXFLOOR; tempFloor++)
+		{
+			upTime++;//上升一层时间加1
+			if (tempFlag == 0 && (upCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				upTime += 3;//停靠一层时间加3
+				aimTempFloor_up = tempFloor;
+				tempFlag = 1;
+			}
+			if (tempFlag = 1 && (upCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				upTime += 3;//停靠一层时间加3
+			}
+		}
+
+		//遍历下部楼层
+		tempFlag = 0;
+		for (tempFloor = curFloor; tempFloor >= 1; tempFloor--)
+		{
+			downTime++;//下降一层时间加1
+			if (tempFlag == 0 && (downCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				downTime += 3;//停靠一层时间加3
+				aimTempFloor_down = tempFloor;
+				tempFlag = 1;
+			}
+			if (tempFlag == 1 && (downCmd[tempFloor - 1] == TRUE || innerCmd[tempFloor - 1] == TRUE))
+			{
+				downTime += 3;//停靠一层时间加3
+			}
+
+		}
+		if (aimTempFloor_up == 0 && aimTempFloor_down == 0)
+			aimFloor = 0;//无上下行命令
+		else if (aimTempFloor_up == 0 && aimTempFloor_down != 0)
+			aimFloor = aimTempFloor_down;//只有下行命令
+		else if (aimTempFloor_up != 0 && aimTempFloor_down == 0)
+			aimFloor = aimTempFloor_up;//只有上行命令
+
+		if (prestate == UP && (aimTempFloor_up != 0 && aimTempFloor_down != 0) && downTime * 5 >= upTime * 6)
+
+	}
+}
--- 电梯设计/main.c
+++ 电梯设计/main.c
@@ -33,7 +33,7 @@ main()
 	system("color 3f");
 	puts("\t\t\tWelcome to 1513-A elevator");
 	puts("Please select the policy you want to use:");
-	puts("1:First call first use;\t2:No extra effort;");
+	puts("1:First call first use;\n2:No extra effort;\n3:Fastest response;");
 
 	int policy=getchar();
 	fflush(stdin);
@@ -53,6 +53,10 @@ main()
 			control_1(); /*根据控制策略确定下一目标楼层，这在state_trans()中要用到 */
 		else if (policy == '2')
 			control_2();
+		else if (policy == '3')
+			control_3();
+	//	else if (policy == '4')
+	//		control_4();
 
 		state_trans(); //根据自动机模型决定此刻电梯的状态 
 
